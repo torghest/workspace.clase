9b3fcc6a304f0b4b1e9e0b2ce983546f0d76e952
alumno <alumno@A03CLI13>
2014-03-31 14:59:01 +0200

    a

---
+++ PropuestoAndroid1/src/com/example/propuestoandroid1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.propuestoandroid1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+public class MainActivity extends Activity {
+	
+	private Spinner spCurso;
+	private EditText edNombre;
+	private EditText edApellido;
+	private EditText edTelefono;
+	private RadioGroup rdHorario;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		spCurso = (Spinner)findViewById(R.id.spCurso);
+		edNombre = (EditText)findViewById(R.id.edNombre);
+		edApellido = (EditText)findViewById(R.id.edApellido);
+		edTelefono = (EditText)findViewById(R.id.edTelefono);
+		rdHorario = (RadioGroup)findViewById(R.id.rdHorario);
+		// Rellenar el Combo
+		String[] opciones = getResources().getStringArray(R.array.datCurso);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				this, R.id.spCurso, opciones);
+		spCurso.setAdapter(adapter);
+	}
+	
+	public void enviar(View view){
+		String curso = spCurso.getSelectedItem().toString();
+	}
+	
+}
---
+++ PropuestoAndroid1/src/com/example/propuestoandroid1/Ventana2Activity.java
@@ -0,0 +1,7 @@
+package com.example.propuestoandroid1;
+
+import android.app.Activity;
+
+public class Ventana2Activity extends Activity {
+
+}
