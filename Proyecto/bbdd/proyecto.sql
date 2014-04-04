06602d70738bc67adcfbf3f646e2206ea74e6f42
alumno <alumno@A03CLI13>
2014-04-04 14:00:18 +0200

    a

--- Proyecto/bbdd/proyecto.sql
+++ Proyecto/bbdd/proyecto.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: localhost
--- Tiempo de generación: 04-04-2014 a las 09:29:34
+-- Tiempo de generación: 04-04-2014 a las 11:59:34
 -- Versión del servidor: 5.5.24-log
 -- Versión de PHP: 5.3.13
 
@@ -29,7 +29,6 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `menu` (
   `id` int(4) NOT NULL AUTO_INCREMENT,
   `Nombre` varchar(20) NOT NULL,
-  `pagina` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `Nombre` (`Nombre`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
@@ -38,14 +37,14 @@ CREATE TABLE IF NOT EXISTS `menu` (
 -- Volcado de datos para la tabla `menu`
 --
 
-INSERT INTO `menu` (`id`, `Nombre`, `pagina`) VALUES
-(1, 'Inicio', 'Inicio'),
-(2, 'Menu.2', 'Menu.2\n'),
-(3, 'Menu.3', 'Menu.3\n'),
-(4, 'Menu.4', 'Menu.4'),
-(5, 'Menu.5', 'Menu.5'),
-(6, 'Menu.6', 'Menu.6'),
-(7, 'Menu.7', 'Menu.7');
+INSERT INTO `menu` (`id`, `Nombre`) VALUES
+(1, 'Inicio'),
+(2, 'Menu.2'),
+(3, 'Menu.3'),
+(4, 'Menu.4'),
+(5, 'Menu.5'),
+(6, 'Menu.6'),
+(7, 'Menu.7');
 
 -- --------------------------------------------------------
 
@@ -56,7 +55,7 @@ INSERT INTO `menu` (`id`, `Nombre`, `pagina`) VALUES
 CREATE TABLE IF NOT EXISTS `submenu` (
   `id` int(4) NOT NULL,
   `nombre` varchar(20) NOT NULL,
-  `pagina` varchar(20) NOT NULL,
+  `jspf` varchar(40) NOT NULL,
   `id_padre` int(4) NOT NULL,
   KEY `id_padre` (`id_padre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -65,10 +64,10 @@ CREATE TABLE IF NOT EXISTS `submenu` (
 -- Volcado de datos para la tabla `submenu`
 --
 
-INSERT INTO `submenu` (`id`, `nombre`, `pagina`, `id_padre`) VALUES
-(1, 'SubMenu.2.1', 'SubMenu.2.1', 2),
-(2, 'SubMenu.2.2', 'SubMenu.2.2', 2),
-(3, 'SubMenu.2.3', 'SubMenu.2.3', 2),
+INSERT INTO `submenu` (`id`, `nombre`, `jspf`, `id_padre`) VALUES
+(1, 'SubMenu.2.1', 'hola.jspf', 2),
+(2, 'SubMenu.2.2', 'WEB-INF/jspf/fragmento.jspf', 2),
+(3, 'SubMenu.2.3', 'WEB-INF/jspf/fragmento.jspf', 2),
 (1, 'SubMenu.3.1', 'SubMenu.3.1', 3),
 (1, 'SubMenu.4.1', 'SubMenu.4.1', 4),
 (2, 'SubMenu.4.2', 'SubMenu.4.2', 4),
@@ -83,7 +82,8 @@ INSERT INTO `submenu` (`id`, `nombre`, `pagina`, `id_padre`) VALUES
 (4, 'SubMenu.6.4', 'SubMenu.6.4', 6),
 (1, 'SubMenu.7.1', 'SubMenu.7.1', 7),
 (2, 'SubMenu.7.2', 'SubMenu.7.2', 7),
-(3, 'SubMenu.7.3', 'SubMenu.7.3', 7);
+(3, 'SubMenu.7.3', 'SubMenu.7.3', 7),
+(1, 'Inicio', 'WEB-INF/jspf/fragmento.jspf', 1);
 
 --
 -- Restricciones para tablas volcadas
